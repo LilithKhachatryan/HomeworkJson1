9598ef479935d6f5981fb3af57d662c61edce90d
Lilit Khachatryan <lilithkhachatryan3@gmail.com>
2024-12-18 01:23:19 +0400

    Added methods to save and read data in JSON format

--- HomeworkJson1/Program.cs
+++ HomeworkJson1/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using HomeworkJson1.Models;
+using HomeworkJson1.Utils;
+
+
+namespace UniversityApp
+{
+    class Program
+    {
+        private const string FilePath = "data.json";
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student { Id = 1, FullName = "Lilit Khachatryan", Age = 21},
+                new Student { Id = 2, FullName = "Nelly Hakobyan", Age = 22 }
+            };
+
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { EmployeeId = 1, Name = "Michael", Department = "IT" }
+            };
+
+            University uni = new University { Name = "YSU", Location = "Yerevan" };
+
+            Address address = new Address { Street = "Shahumyan", City = "Ararat", Country = "Armenia" };
+
+            JsonHelper.WriteToFile(FilePath, students);
+            JsonHelper.WriteToFile(FilePath, employees);
+
+            Console.WriteLine("Data saved to JSON.");
+
+            List<Student> loadedStudents = JsonHelper.ReadFromFile<Student>(FilePath);
+
+            foreach (var student in loadedStudents)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ HomeworkJson1/Utils/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+
+namespace HomeworkJson1.Utils
+{
+    public static class JsonHelper
+    {
+        public static void WriteToFile<T>(string filePath, List<T> items)
+        {
+            string jsonString = JsonSerializer.Serialize(items, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public static List<T> ReadFromFile<T>(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                string jsonString = File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<List<T>>(jsonString);
+            }
+            return new List<T>();
+        }
+    }
+}
